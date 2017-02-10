3b8fac4507f7fdb472cc5417b6b37857362b8e05
inginginger <umka.91@mail.ru>
2017-02-10 08:21:30 +0300

    new modules

---
+++ answerLCS.v
@@ -0,0 +1,95 @@
+module answerLCS(
+	input clk,
+	input edgeTx,
+	input rst,
+	input SW, 
+	input [8:0] addrLCS,
+	input req,
+	input [7:0] dataTemp,
+	input [7:0] dataLCS,
+	output reg ack,
+	output  [7:0] dataTx,
+	output  [6:0] addrTemp
+	);
+	
+	reg [1:0] syncSW;
+	reg [1:0] syncReq;
+	reg [1:0] syncEdge;
+	reg [2:0] state;
+	reg [1:0] cntTemp;
+	reg [2:0] shiftByte;
+	reg enTemp;
+	reg [3:0] cnt;
+	
+	assign dataTx = (enTemp == 1'b1) ? dataTemp : dataLCS;
+	assign addrTemp = cntTemp  + (shiftByte << 2);
+	
+	localparam IDLE = 0, EDGE = 1, CHECK = 2, WAIT = 3, DELAY = 4;
+
+	always@(posedge edgeTx) begin
+		syncSW <= {syncSW[0], SW};
+		syncReq <= {syncReq[0], req};
+		//syncEdge <= {syncEdge[0], edgeTx};
+	end
+
+	always@(posedge edgeTx or negedge rst) begin
+		if(~rst) begin
+			ack <= 1'b0;
+			state <= 3'd0;
+			cntTemp <= 2'd0;
+			shiftByte <= 3'd0;
+			enTemp <= 1'b0;
+			cnt <= 4'd0;
+		end else begin
+			case(state)
+				IDLE: begin
+					if(syncReq[1]) begin	//если приняли запрос на данные
+						ack <= 1'b1;		//формируем подтверждение
+						state <= CHECK;
+					end
+				end
+				/*EDGE: begin
+					if(syncEdge[1]) begin
+						state <= CHECK;
+					end
+				end*/
+				CHECK: begin
+					if(syncSW[1] && (addrLCS == 184 || addrLCS == 185	//если переключились памяти для записи кадра
+						|| addrLCS == 186 || addrLCS == 187)) begin	//и все быстрые параметры уже в кадре
+						enTemp <= 1'b1;
+						cntTemp <= cntTemp + 1'b1;
+						if(cntTemp == 2'd3) begin
+							shiftByte <= shiftByte + 1'b1;
+							state <= DELAY;
+						/*end else begin
+							state <= WAITEDGE;*/
+						end
+						//state <= IDLE;
+					end else begin
+						enTemp <= 1'b0;									//отдаем данные ялк
+						state <= DELAY;
+					end	
+								
+				end
+				DELAY: begin
+					cnt <= cnt + 1'b1;
+					if(cnt == 4'd9) begin
+						cnt <= 4'd0;
+						state<= WAIT;
+					end
+				end
+				/*WAITEDGE: begin
+					if(~syncEdge[1]) begin
+						state <= IDLE;
+					end
+				end*/
+				WAIT: begin
+					if(~syncReq[1]) begin
+						state <= IDLE;
+						ack <= 1'b0;
+					end
+				end
+			endcase
+		end
+	end
+endmodule
---
+++ newUart.v
@@ -0,0 +1,169 @@
+module newUart
+#(
+  parameter BYTES = 5'd4
+)
+(
+  input reset,          // global reset and enable signal
+  input clk,            // actual needed baudrate (tested on 4,8 MHz)
+  input RQ,            // start transfer signal
+  input ack,
+  input edgeTx,
+  input [5:0]cycle,
+  input [7:0]data,
+  output [8:0]addr,
+  output reg full,
+  output reg rqRom,
+  output reg tx,          // serial transmitted data
+  output reg dirTX,        // rs485 TX dir controller 
+  output reg dirRX,        // rs485 RX dir controller
+  output reg [1:0]switch  // memory switcher
+);
+
+//  wire [7:0]data;        // 8bits data
+  
+//  wire [8:0]addr;
+  //reg unsigned [4:0]cycle;
+
+  
+  
+//dROM innerMEM(
+//  .aclr(!reset),
+//  .address(addr),
+//  .clock(clk),
+//  .q(data));
+
+localparam WAIT=0, RQROM = 1, ACK = 2, MEGAWAIT=3, DIRON=4, TX=5, DIROFF=6, EDGE = 7;
+
+reg [2:0] state;
+reg [3:0] serialize;
+reg [4:0] delay;
+reg [1:0] rqsync;
+reg [1:0] syncEdge;
+reg [1:0] syncAck;
+reg [7:0] bufTemp;
+reg txOn;
+
+assign addr = (switch + (cycle << 2));
+
+always@(posedge edgeTx) begin			// double d-flipflop to avoid metastability
+	rqsync <= {rqsync[0],  RQ};	// start signal from other clock domain
+	syncAck <= {syncAck[0], ack};
+	//syncEdge <= {syncEdge[0], edgeTx};
+end
+
+always@(posedge edgeTx or negedge reset)
+begin
+if (~reset) begin					// global asyncronous reset, initial values
+	state <= 3'b0;
+	serialize <= 0;
+	delay <= 1'b0;
+	tx <= 1'b1;
+	switch <= 2'd0;
+	full <= 1'b0;
+	rqRom <= 1'b0;
+	bufTemp <= 8'd0;
+	dirRX <= 1'b0;
+	dirTX <= 1'b0;
+	txOn <= 1'b0;
+end else begin						// main circuit
+	
+	case (state)					// state machine
+		WAIT: begin					// waiting for transfer request
+			full <= 1'b0;
+			if (rqsync[1]) begin 
+				state <= RQROM;
+			end
+		end
+		RQROM: begin
+			rqRom <= 1'b1;
+			bufTemp <= data;
+			state <= ACK;		// just move on
+		end
+		ACK: begin
+			if(syncAck[1]) begin
+				state <= DIRON;
+			end
+		end
+		/*EDGE: begin
+			if(syncEdge[1]) begin
+				if(txOn == 1'b1) begin
+					state <= TX;
+				end else begin
+					state <= DIRON;
+				end
+			end
+			
+		end*/
+		DIRON: begin 				// set the DIR pins to high level with a tiny delay
+			delay <= delay + 1'b1;	// count while in this state
+			//state <= EDGE;
+			if (delay == 0) begin 
+				dirRX <= 1; 
+				
+			end
+			if (delay == 15) begin 
+				dirTX <= 1; 
+				
+			end
+			if (delay == 30) begin 
+				state <= TX; 
+				rqRom <= 1'b0;
+				txOn <= 1'b1;
+				
+			end	// proceed to next state
+			//switch <= 0; 
+			
+		end
+		TX: begin					// the transfer
+			serialize <= serialize + 1'b1;		// count while in this state
+			//state <= EDGE;
+			case (serialize)					// make a sequence while here
+				0: begin 
+					tx <= 0;  		// startbit
+					delay <= 0;		// reset previous counter
+					
+				end
+				1,2,3,4,5,6,7,8: begin
+					tx <= data[(serialize - 1)];	// transmit every bit of data
+				end
+				9: begin 
+					tx <= 1;		// stopbit
+					switch <= switch + 1'b1;	// switch memory
+					serialize <= 0; // reset sequencer
+					if (switch == BYTES-1) begin 
+						state <= DIROFF; 
+						
+					end	
+				end
+				//10: begin
+					// if completed transfer proceed to next state 
+					/*else begin
+						state <= RQROM;
+					end*/
+				//end	
+			endcase
+			
+			
+		end
+		DIROFF: begin				// set the DIR pins to low level with a tiny delay
+			txOn <= 1'b0;
+			delay <= delay + 1'b1;	// count while in this state
+			if (delay == 0) begin 
+				dirTX <= 0; 
+			end else if (delay == 4) begin 
+				dirRX <= 0; 
+				state <= MEGAWAIT; 
+				full <= 1'b1; 
+			end	// proceed to next state
+			//state <= EDGE;
+		end
+		MEGAWAIT: begin			// checking the low level of request signal
+			delay <= 0;				// reset previous counter
+			if (~rqsync[1]) begin 
+				state <= WAIT; // just move on
+			end
+		end
+	endcase 
+end
+end
+endmodule
