553162aa56faa20bdb1d0a9c7fe4a3c9988503bf
inginginger <umka.91@mail.ru>
2017-02-17 14:35:32 +0300

    чушь в симуляции, перескакивают значения из rom, хотя адресация идет по порядку

--- answerLCS.v
+++ answerLCS.v
@@ -17,12 +17,12 @@ module answerLCS(
 	reg [1:0] syncEdge;
 	reg [2:0] state;
 	reg [1:0] cntTemp;
-	reg [2:0] shiftByte;
+	reg [4:0] shiftByte;
 	reg enTemp;
 	reg [3:0] cnt;
 	
-	assign dataTx = (enTemp == 1'b1) ? dataTemp : dataLCS;
-	assign addrTemp = (cntTemp - 1)  + (shiftByte << 2);
+	assign addrTemp = ((cntTemp == 0) && (shiftByte == 0))? 7'd127 :((cntTemp-1)  + (shiftByte << 2));
+	assign dataTx = /*(enTemp == 1'b1) ? dataTemp : */dataLCS;
 	
 	localparam IDLE = 0, EDGE = 1, CHECK = 2, WAIT = 3, DELAY = 4;
 
@@ -37,10 +37,11 @@ module answerLCS(
 			ack <= 1'b0;
 			state <= 3'd0;
 			cntTemp <= 2'd0;
-			shiftByte <= 3'd0;
+			shiftByte <= 5'd0;
 			enTemp <= 1'b0;
 			cnt <= 4'd0;
 		end else begin
+			
 			case(state)
 				IDLE: begin
 					if(req) begin	//если приняли запрос на данные
@@ -48,46 +49,32 @@ module answerLCS(
 						state <= CHECK;
 					end
 				end
-				/*EDGE: begin
-					if(syncEdge[1]) begin
-						state <= CHECK;
-					end
-				end*/
+
 				CHECK: begin
-					if(syncSW[1] && (addrLCS == 184 || addrLCS == 185	//если переключились памяти для записи кадра
-						|| addrLCS == 186 || addrLCS == 187)) begin	//и все быстрые параметры уже в кадре
+					if(syncSW[1] && (addrLCS == 184 || addrLCS == 185
+						|| addrLCS == 186 || addrLCS == 187)) begin	
 						enTemp <= 1'b1;
 						state <= DELAY;
 						ack <= 1'b0;
 						cntTemp <= cntTemp + 1'b1;
 						if(cntTemp == 2'd3) begin
-							//ack <= 1'b0;
-							shiftByte <= shiftByte + 1'b1;
-							
-						/*end else begin
-							state <= WAITEDGE;*/
+							shiftByte <= shiftByte + 1'b1;	
 						end
-						//state <= IDLE;
 					end else begin
 						ack <= 1'b0;
 						enTemp <= 1'b0;									//отдаем данные ялк
 						state <= DELAY;
-					end	
-					
-								
+					end								
 				end
+				
 				DELAY: begin
 					cnt <= cnt + 1'b1;
 					if(cnt == 4'd9) begin
 						cnt <= 4'd0;
-						state<= WAIT;
+						state <= WAIT;
 					end
 				end
-				/*WAITEDGE: begin
-					if(~syncEdge[1]) begin
-						state <= IDLE;
-					end
-				end*/
+
 				WAIT: begin
 					if(~req) begin
 						state <= IDLE;
